9399f93fecf3914ec4226211a186b2024776b71a
jh64 <48374211+jh64@users.noreply.github.com>
2020-10-17 19:42:00 +1100

    refine

--- _buildMe.sh
+++ _buildMe.sh
@@ -1,23 +1,29 @@
 echo ++++++++++++++++++++++++++++++++++++++
-docker rmi my-app-ws:SNAPSHOT --force
+docker rmi my-app-ws:0.0.1-SNAPSHOT --force
 docker image ls
-read -p "Press Enter to continue..."
+#read -p "Press Enter to continue..."
 
 echo ++++++++++++++++++++++++++++++++++++++
-#sudo ./mvnw                spring-boot:build-image
- sudo ./mvnw         -Pprod spring-boot:build-image
+ sudo ./mvnw                spring-boot:build-image
+#sudo ./mvnw         -Pprod spring-boot:build-image
 
 #sudo ./mvnw package -Pprod verify jib:dockerBuild
 #sudo ./mvnw package        verify jib:dockerBuild
 
 docker image ls
-read -p "Press Enter to continue..."
+#read -p "Press Enter to continue..."
 
 echo ++++++++++++++++++++++++++++++++++++++
+#docker rmi                                u20041-kworker3-dev:5000/my-app-ws:0.0.1-PROD
+#docker image tag my-app-ws:0.0.1-SNAPSHOT u20041-kworker3-dev:5000/my-app-ws:0.0.1-PROD
+
 docker rmi                                u20041-kworker3-dev:5000/my-app-ws:0.0.1-SNAPSHOT
-docker image tag my-app-ws:0.0.1-SNAPSHOT u20041-kworker3-dev:5000/my-app-ws:0.0.1-PROD
+docker image tag my-app-ws:0.0.1-SNAPSHOT u20041-kworker3-dev:5000/my-app-ws:0.0.1-SNAPSHOT
+
 docker image ls
-read -p "Press Enter to continue..."
+#read -p "Press Enter to continue..."
 
 echo ++++++++++++++++++++++++++++++++++++++
-docker push u20041-kworker3-dev:5000/my-app-ws:0.0.1-PROD
+#docker push u20041-kworker3-dev:5000/my-app-ws:0.0.1-PROD
+
+docker push u20041-kworker3-dev:5000/my-app-ws:0.0.1-SNAPSHOT
